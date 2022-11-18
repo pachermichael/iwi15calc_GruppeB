572e3771bea0d31af5edb1bedfe78dfd2fa52bc2
Xiangjun.Wu <s51043@edu.campus02.at>
2022-11-18 18:02:18 +0100

    E2E Test implementiert

---
+++ stackCalculator/src/test/java/at/edu/c02/calculator/end2end/End2EndTest.java
@@ -0,0 +1,44 @@
+package at.edu.c02.calculator.end2end;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import at.edu.c02.calculator.parser.Parser;
+import org.junit.Test;
+
+import at.edu.c02.calculator.Calculator;
+import at.edu.c02.calculator.CalculatorException;
+import at.edu.c02.calculator.Calculator.Operation;
+import at.edu.c02.calculator.logic.CalculatorImpl;
+
+import java.io.File;
+
+public class End2EndTest {
+
+    @Test
+    public void testMultipleOperations1() throws Exception {
+
+        //setup
+        Calculator calc = new CalculatorImpl();
+
+        Parser parser = new Parser(calc);
+        //execute
+        double result = parser.parse(new File("src/test/resources/test05.xml"));
+
+        //verify
+        assertEquals(14, result, 0);
+    }
+
+    @Test
+    public void testMultipleOperations2() throws Exception {
+
+        //setup
+        Calculator calc = new CalculatorImpl();
+        Parser parser = new Parser(calc);
+        //execute
+        double result = parser.parse(new File("src/test/resources/test06.xml"));
+
+        //verify
+        assertEquals(1, result, 0);
+    }
+}
